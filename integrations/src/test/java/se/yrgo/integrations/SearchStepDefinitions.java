052dfb5dfe65af1fb12ce05b83fe3347ea19114c
jimmsi <jimmyem@gmail.com>
2024-11-18 14:10:44 +0100

    Selenium and Cucumber setup complete

---
+++ integrations/src/test/java/se/yrgo/integrations/SearchStepDefinitions.java
@@ -0,0 +1,13 @@
+package se.yrgo.integrations;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SearchStepDefinitions {
+    @When("the user navigates to the book search.")
+    public void the_user_navigates_to_the_book_search() {
+    }
+    @Then("they can see the search form.")
+    public void they_can_see_the_search_form() {
+    }
+}
